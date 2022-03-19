f88e17b17b34f6072a14d965bfc335f5ab0aa8f4
Enzo FATEC <enzo.gerola@fatec.sp.gov.br>
2022-03-19 18:14:56 -0300

    [appmax][produtos] - Implementando funções de: cadastrar, alterar, buscar e deletar produtos.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,4 +38,16 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Throwable $exception)
+    {
+        return parent::render($request, $exception);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+/**
+ * Declare the main resource controller
+ */
+Route::apiResource('/products', 'ProductsController');
